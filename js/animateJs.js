481ed6c32ba332fd5550f24017489265b95c54b4
chen-wei-ni <bcg2135@gmail.com>
2022-03-15 17:07:39 +0800

    first upload

---
+++ js/animateJs.js
@@ -0,0 +1,48 @@
+ScrollReveal().reveal('.scroll__event', {delay: 500 ,opacity: 0.05 ,duration: 2000})
+ScrollReveal().reveal('.ex-title', {delay: 500 ,opacity: 0.05})
+ScrollReveal().reveal('.slide__wrap', {delay: 1000 ,origin : 'right',reset: true, desktop: true, opacity: 0.05, duration: 2000})
+
+
+//SWIPER
+
+    const swiper = new Swiper('.swiper', {
+        // Default parameters
+        slidesPerView: 1.5,
+        spaceBetween: 0,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
+        // Responsive breakpoints
+        breakpoints: {
+          // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 25,
+                initialSlide: 3,
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2.2,
+                spaceBetween: 10
+            },
+            767: {
+                slidesPerView: 3.2,
+                spaceBetween: 20
+            },
+            // when window width is >= 920px
+            920: {
+                slidesPerView: 5,
+                spaceBetween: 0
+                
+            }
+            
+        }
+        
+        })
+
+
