c742ab1faf9f8984e39a6a859bb658cd914bb2ec
chen-wei-ni <bcg2135@gmail.com>
2022-03-29 13:14:10 +0800

    add pictures

--- js/animateJs.js
+++ js/animateJs.js
@@ -4,44 +4,64 @@ ScrollReveal().reveal('.slide__wrap', {delay: 1000 ,opacity: 0.05})
 
 //SWIPER
 
-    const swiper = new Swiper('.swiper', {
-        // Default parameters
-        slidesPerView: 1.5,
-        spaceBetween: 0,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-            },
-        pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 1.5,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
         },
-        // Responsive breakpoints
-        breakpoints: {
-          // when window width is >= 320px
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 25,
-                initialSlide: 3,
-            },
-            // when window width is >= 480px
-            480: {
-                slidesPerView: 2.2,
-                spaceBetween: 10
-            },
-            767: {
-                slidesPerView: 3.2,
-                spaceBetween: 20
-            },
-            // when window width is >= 920px
-            920: {
-                slidesPerView: 5,
-                spaceBetween: 0
-                
-            }
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 25,
+            initialSlide: 3,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2.2,
+            spaceBetween: 10
+        },
+        767: {
+            slidesPerView: 3.2,
+            spaceBetween: 20
+        },
+        // when window width is >= 920px
+        920: {
+            slidesPerView: 5,
+            spaceBetween: 0
             
         }
         
-        })
+    }
+    })
+
+
+
+var swiper2 = new Swiper(".mySwiper-2", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    spaceBetween: 10,
+    keyboard: {
+        enabled: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+
+    });
 
 
